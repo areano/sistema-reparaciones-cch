71e8dfe29e71d0e62a20c1cfc89cb2e8712b4a2f
jair.tabares <jair.tabares@eaebc475-0cab-368d-42bf-08e92b9635b1>
2013-08-17 21:23:18 +0000

    create cliente support for hibernate

---
+++ src/com/uade/seminario/tpo/persistence/dao/generic/impl/ClienteDAOImpl.java
@@ -0,0 +1,29 @@
+package com.uade.seminario.tpo.persistence.dao.generic.impl;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import com.uade.seminario.tpo.model.Cliente;
+import com.uade.seminario.tpo.persistence.hbt.HibernateUtil;
+
+public class ClienteDAOImpl extends GenericDAOImpl<Cliente>{
+	private static ClienteDAOImpl instancia = null;
+	private static SessionFactory sf = null;
+	public static ClienteDAOImpl getInstancia(){
+		if(instancia == null){
+			sf = HibernateUtil.getSessionFactory();
+			instancia = new ClienteDAOImpl();
+		} 
+		return instancia;
+	}
+
+	public Cliente findByDNI(int nroDoc, String tipoDoc) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		String hql = "from Cliente p where p.nroDoc = :nroDoc and p.tipoDoc = tipoDoc";
+		Query query = session.createQuery(hql);
+		query.setParameter("nroDoc", nroDoc);
+		query.setParameter("tipoDoc", tipoDoc);		
+		return (Cliente)query.uniqueResult();
+	}
+
+}
---
+++ src/com/uade/seminario/tpo/service/ClienteService.java
@@ -0,0 +1,28 @@
+package com.uade.seminario.tpo.service;
+
+import com.uade.seminario.tpo.model.Cliente;
+import com.uade.seminario.tpo.persistence.dao.generic.impl.ClienteDAOImpl;
+
+public class ClienteService {
+	private static ClienteDAOImpl dao;
+	private static ClienteService cs=null;
+	static {
+		dao = ClienteDAOImpl.getInstancia();
+	}
+
+	private ClienteService(){
+		
+	}
+	public static ClienteService getInstance(){
+		if (cs==null){
+			cs =  new ClienteService();
+		}
+		return cs;
+	}
+	public Cliente findByDNI(int nroDoc,String tipoDoc){
+		return dao.findByDNI(nroDoc, tipoDoc);
+	}
+	public void save(Cliente cliente){
+		dao.save(cliente);
+	}
+}
